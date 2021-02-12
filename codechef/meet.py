2d7df536570ce6f89872f21ce5b216b659a6b541
Aditi Jha <aditi124jha@gmail.com>
2021-02-12 08:33:07 +0530

    added files

--- codechef/meet.py
+++ codechef/meet.py
@@ -1,12 +1,31 @@
-import re
-def main():
-    t = int(input())
-    for _ in range(t):
-        p=input()
-        n=int(input())
-        for _ in range(n):
-            i=input()
-            l,r=re.split('AM PM',i)
-            print (l,r)
-    return 0
-main()
+def times(c):
+    o=0
+    if "AM" in c:
+        if c[:2]!="12":
+            o=int(c[:2])*60+int(c[3:5])
+        else:
+            o=int(c[3:5])
+    else:
+        if c[:2]!="12":
+            o=(int(c[:2])+12)*60+int(c[3:5])
+        else:
+            o=720+int(c[3:5])
+    return o
+
+t = int(input())
+for _ in range(t):
+    op=""
+    p=input()
+    p=times(p)
+    n=int(input())
+    for _ in range(n):
+        time=input()
+        l=times(time[:8])
+        r=times(time[9:])
+        if p>=l and p<=r:
+            op+="1"
+        elif r<l and p>=r and p<=l:
+            op=+"1"
+        else:
+            op+="0"
+    print(op) 
